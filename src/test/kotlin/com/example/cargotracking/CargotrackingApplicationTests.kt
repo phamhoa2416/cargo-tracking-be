21bc36aeff9735dfbdc2d2a0ff58e630bfd267d5
phamhoa2416 <phamviethoa2416@gmail.com>
2025-12-18 13:24:51 +0700

    init project

---
+++ src/test/kotlin/com/example/cargotracking/CargotrackingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cargotracking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CargotrackingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
