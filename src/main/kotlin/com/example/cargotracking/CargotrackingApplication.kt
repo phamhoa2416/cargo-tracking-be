47b6916a2d1774a9f64fbaeca8de3dbcb90e27e0
phamviethoa <phamviethoa2416@gmail.com>
2025-12-28 23:39:22 +0700

    fix: rename to match environment variable

--- src/main/kotlin/com/example/cargotracking/CargotrackingApplication.kt
+++ src/main/kotlin/com/example/cargotracking/CargotrackingApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CargoTrackingApplication
+class CargotrackingApplication
 
 fun main(args: Array<String>) {
-	runApplication<CargoTrackingApplication>(*args)
+	runApplication<CargotrackingApplication>(*args)
 }
